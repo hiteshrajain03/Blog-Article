02a432070a96b94f4919b89cf9a81f30b81af661
hiteshrajain03 <hiteshrajain03@gmail.com>
2023-04-20 00:11:31 +0530

    dotenv added

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
+require('dotenv').config();
 
-mongoose.connect('mongodb://localhost/blog', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
 
